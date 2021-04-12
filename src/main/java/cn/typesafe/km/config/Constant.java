bf9414ae76178853adfa0cc32ab4ef11881fc6fb
dushixiang <helloworld1024@foxmail.com>
2021-04-12 21:34:58 +0800

    修改平台使用的consumer group id

--- src/main/java/cn/typesafe/km/config/Constant.java
+++ src/main/java/cn/typesafe/km/config/Constant.java
@@ -5,5 +5,5 @@ package cn.typesafe.km.config;
  * @date 2021/4/10 3:17 下午
  */
 public final class Constant {
-    public static final String CONSUMER_GROUP_ID = "kafka-dashboard";
+    public static final String CONSUMER_GROUP_ID = "kafka-map";
 }
