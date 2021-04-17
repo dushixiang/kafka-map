122c295c6cef8def50974e54d68f453f2624b489
dushixiang <798148596@qq.com>
2021-04-17 23:29:07 +0800

    增加日志配置文件和延迟消息单元测试

--- src/test/java/cn/typesafe/km/ProducerTest.java
+++ src/test/java/cn/typesafe/km/ProducerTest.java
@@ -23,15 +23,7 @@ public class ProducerTest {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.1.5.84:9094");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-//        props.put(ProducerConfig.ACKS_CONFIG)
-
-        //request.required.acks
-        //0, which means that the producer never waits for an acknowledgement from the broker (the same behavior as 0.7). This option provides the lowest latency but the weakest durability guarantees (some data will be lost when a server fails).
-        //1, which means that the producer gets an acknowledgement after the leader replica has received the data. This option provides better durability as the client waits until the server acknowledges the request as successful (only messages that were written to the now-dead leader but not yet replicated will be lost).
-        //-1, which means that the producer gets an acknowledgement after all in-sync replicas have received the data. This option provides the best durability, we guarantee that no messages will be lost as long as at least one in sync replica remains.
-//        props.put("request.required.acks","-1");
-
-        producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(props);
+        producer = new org.apache.kafka.clients.producer.KafkaProducer<>(props);
     }
 
     @Test
