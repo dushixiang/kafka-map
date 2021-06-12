4826b323a1c54ab078bcc60e0822b001825d4a45
dushixiang <helloworld1024@foxmail.com>
2021-06-12 18:04:56 +0800

    完善README

--- src/main/java/cn/typesafe/km/controller/AccountController.java
+++ src/main/java/cn/typesafe/km/controller/AccountController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
