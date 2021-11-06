8746ba5eb007f9fed7128bc23af96b2478a09286
dushixiang <helloworld1024@foxmail.com>
2021-11-06 22:13:26 +0800

    修改打包方式

--- build.sh
+++
@@ -1,6 +0,0 @@
-cd web && npm run build
-mv build ../src/main/resources/static
-source ~/.bash_profile
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.7.jdk/Contents/Home
-cd .. && mvn clean package -Dmaven.test.skip=true
-rm -rf src/main/resources/static
--- pre_build_docker.sh
+++
@@ -1,2 +0,0 @@
-cp target/kafka-map-*.jar docker/kafka-map.jar
-cp LICENSE docker/LICENSE
